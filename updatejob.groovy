17b58c31327fb516878ed14e0b21ae9bacb8b899
Windows <jupamarin@gmail.com>
2026-04-29 11:42:31 -0500

    cambios cobertura

--- updatejob.groovy
+++
@@ -1,7 +0,0 @@
-import org.jenkinsci.plugins.workflow.job.*
-import org.jenkinsci.plugins.workflow.cps.*
-
-def job = jenkins.model.Jenkins.instance.getItem('Ficha-Caracterizacion-Pipeline-QA')
-def newScript = new File('/tmp/Jenkinsfile').text
-job.definition = new CpsFlowDefinition(newScript, true)
-job.save()
